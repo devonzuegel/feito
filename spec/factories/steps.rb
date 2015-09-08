9d11b133787beb4bd2bbb56000694e59e2093565
Devon Kristine Zuegel <devonzuegel@gmail.com>
2015-09-08 16:16:01 -0700

    All specs passing again

--- spec/factories/steps.rb
+++ spec/factories/steps.rb
@@ -1,9 +1,8 @@
 FactoryGirl.define do
   factory :step do
-    title "MyString"
-due "2015-09-08 15:12:33"
-completed false
-task nil
+    title 'MyString'
+    due '2015-09-08 15:12:33'
+    completed false
+    task nil
   end
-
 end
